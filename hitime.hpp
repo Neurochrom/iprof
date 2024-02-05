1d546f879e9b33a16109608f76539971adcf24fd
Neurochrom <pcichock@poczta.onet.pl>
2024-02-05 14:50:18 +0100

    Prevent iprof from defining min and max through Windows.h.

--- hitime.hpp
+++ hitime.hpp
@@ -7,7 +7,28 @@
 
 #ifdef _MSC_VER
 
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#define UNDEF_WIN32_LEAN_AND_MEAN
+#endif
+
+#ifndef NOMINMAX
+#define NOMINMAX
+#define UNDEF_NOMINMAX
+#endif
+
 #include <Windows.h>
+
+#ifdef UNDEF_WIN32_LEAN_AND_MEAN
+#undef WIN32_LEAN_AND_MEAN
+#undef UNDEF_WIN32_LEAN_AND_MEAN
+#endif
+
+#ifdef UNDEF_NOMINMAX
+#undef NOMINMAX
+#undef UNDEF_NOMINMAX
+#endif
+
 namespace
 {
    const long long g_Frequency = []() -> long long
